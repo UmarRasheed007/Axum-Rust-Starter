55537fb45437044c16452fdb89007fd0afbfeae5
Umar Rasheed <umar.rasheed@codora.io>
2024-11-12 17:52:24 +0500

    fix: issue in main file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use axum;
+use std::sync::Arc;
+
 use crate::config::database::DatabaseTrait;
 use crate::config::{database, parameter};
-use std::sync::Arc;
 
 mod config;
 mod dto;
@@ -17,14 +19,26 @@ mod state;
 #[tokio::main]
 async fn main() {
     parameter::init();
+    // Initialize tracing for logging
+    tracing_subscriber::fmt::init();
+
+    // Connect to the PostgreSQL database using SeaORM
     let connection = database::Database::init()
         .await
         .unwrap_or_else(|e| panic!("Database error: {}", e.to_string()));
 
-    let host = format!("0.0.0.0:{}", parameter::get("PORT"));
-    tracing_subscriber::fmt::init();
-    axum::Server::bind(&host.parse().unwrap())
-        .serve(routes::root::routes(Arc::new(connection)))
+    // Set up the server
+    let host = format!(
+        "0.0.0.0:{}",
+        std::env::var("PORT").unwrap_or_else(|_| "8080".to_string())
+    );
+
+    // creating and starting the server
+    let app = routes::root::routes(Arc::new(connection));
+    let listener = tokio::net::TcpListener::bind(&host.parse().unwrap())
+        .await
+        .unwrap();
+    axum::serve(listener, app)
         .await
         .unwrap_or_else(|e| panic!("Server error: {}", e.to_string()));
 }
